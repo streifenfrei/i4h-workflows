9aa6351d777d47d3fcc280c505543b86c5486095
Martin Lelis <martin.lelis@nct-dresden.de>
2026-02-23 17:51:56 +0100

    fix correct camera 3

--- workflows/so_arm_starter/scripts/simulation/exts/so_arm_starter_ext/so_arm_starter_ext/tasks/so_arm_starter/approach/config/soarm101/so_arm_env_cfg.py
+++ workflows/so_arm_starter/scripts/simulation/exts/so_arm_starter_ext/so_arm_starter_ext/tasks/so_arm_starter/approach/config/soarm101/so_arm_env_cfg.py
@@ -377,8 +377,8 @@ class SOARMStarterEnvCfg(ManagerBasedRLEnvCfg):
 
         self.decimation = 1
         self.episode_length_s = 8.0
-        self.viewer.eye = (0.25, 1.5, 2.5)
-        self.viewer.lookat = (0.25, 0.1, 0.1)
+        self.viewer.eye = (2.0, 2.0, 1.5)
+        self.viewer.lookat = (0.0, 0.0, 0.2)
         self.actions = init_action_cfg(self.actions, device="keyboard")
 
         self.sim.physx.bounce_threshold_velocity = 0.01
